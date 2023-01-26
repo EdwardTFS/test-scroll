c79bcbc7270d58595567e60a8c0597e9d01502bc
Edziu <goerlich_e@tfs.pl>
2023-01-26 15:21:52 +0100

    initial demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'component1', component: Component1Component},
+  {path: 'component2', component: Component2Component},
+  { path: '**', redirectTo: 'component1' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
